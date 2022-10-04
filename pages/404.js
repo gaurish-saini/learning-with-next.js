9d173bfa214edb0460fded03d6c0fbd37ee46c96
Gaurish Prakhar Saini <gaurishprakhar@gmail.com>
2022-10-05 05:26:45 +0530

    styled JSX

--- pages/404.js
+++ pages/404.js
@@ -3,7 +3,14 @@ import Layout from "../components/Layout";
 export default function NotFound() {
   return (
     <Layout>
-      <h1>Not Found</h1>;
+      <h1>Not Found</h1>
+      <style jsx>
+        {`
+          h1 {
+            color: chocolate;
+          }
+        `}
+      </style>
     </Layout>
   );
 }
